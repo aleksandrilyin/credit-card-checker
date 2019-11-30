3f0283240efe9550c0a5d28a38adbc5404331823
Alexander Ilyin <alexander.ilyin11@gmail.com>
2019-11-30 06:11:48 +0300

    Create app structure

---
+++ lib/credit_card.rb
@@ -0,0 +1,17 @@
+arr = [{
+    card_type: 'AMEX',
+    begins_with: [34, 37],
+    num_length: [15]
+  }, {
+    card_type: 'Discover',
+    begins_with: [6011],
+    num_length: [16]
+  }, {
+    card_type: 'MasterCard',
+    begins_with: [51..55],
+    num_length: [16]
+  }, {
+    card_type: 'Visa',
+    begins_with: [4],
+    num_length: [13, 16]
+  }]
