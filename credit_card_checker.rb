321278876d1b028c44ea452ec12f289c6c374e9b
Alexander Ilyin <alexander.ilyin11@gmail.com>
2019-12-09 08:25:47 +0300

    Add hash and delete credit_card.rb

--- credit_card_checker.rb
+++ credit_card_checker.rb
@@ -1,5 +1,12 @@
-require_relative 'lib/credit_card'
 require_relative 'lib/card_num'
+
+credit_cards = {
+  # card_type: [[begins_with], [num_length]],
+  AMEX: [[34, 37], [15]],
+  Discover: [[6011], [16]],
+  MasterCard: [[51, 52, 53, 54, 55], [16]],
+  Visa: [[4], [13, 16]]
+}
 
 CardNum.check_argument
 
