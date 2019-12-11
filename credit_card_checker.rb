f06cdbeadf60afcff84ac45a2a2c801a49ec9a0a
Alexander Ilyin <alexander.ilyin11@gmail.com>
2019-12-11 23:53:25 +0300

    Return last hash

--- credit_card_checker.rb
+++ credit_card_checker.rb
@@ -1,12 +1,22 @@
 require_relative 'lib/card_num'
 
-credit_cards = {
-  # card_type: [[begins_with], [num_length]],
-  AMEX: [[34, 37], [15]],
-  Discover: [[6011], [16]],
-  MasterCard: [[51, 52, 53, 54, 55], [16]],
-  Visa: [[4], [13, 16]]
-}
+credit_cards = [{
+  card_type: 'AMEX',
+  begins_with: [34, 37],
+  num_length: [15]
+}, {
+  card_type: 'Discover',
+  begins_with: [6011],
+  num_length: [16]
+}, {
+  card_type: 'MasterCard',
+  begins_with: [51, 52, 53, 54, 55],
+  num_length: [16]
+}, {
+  card_type: 'Visa',
+  begins_with: [4],
+  num_length: [13, 16]
+}]
 
 CardNum.check_argument
 
