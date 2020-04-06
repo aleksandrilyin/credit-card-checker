16f47063494bfbc623e600c5ed76391ec8c4741c
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-04-07 01:58:56 +0300

    Add indentation to hash

--- credit_card_checker.rb
+++ credit_card_checker.rb
@@ -1,22 +1,24 @@
 require_relative 'lib/card_num'
 
-credit_cards = [{
-  card_type: :AMEX,
-  begins_with: [34, 37],
-  num_length: [15]
-}, {
-  card_type: :Discover,
-  begins_with: [6011],
-  num_length: [16]
-}, {
-  card_type: :MasterCard,
-  begins_with: [51, 52, 53, 54, 55],
-  num_length: [16]
-}, {
-  card_type: :Visa,
-  begins_with: [4],
-  num_length: [13, 16]
-}]
+credit_cards = [
+  {
+    card_type: :AMEX,
+    begins_with: [34, 37],
+    num_length: [15]
+  }, {
+    card_type: :Discover,
+    begins_with: [6011],
+    num_length: [16]
+  }, {
+    card_type: :MasterCard,
+    begins_with: [51, 52, 53, 54, 55],
+    num_length: [16]
+  }, {
+    card_type: :Visa,
+    begins_with: [4],
+    num_length: [13, 16]
+  }
+]
 
 cn = CardNum.new(ARGV)
 
