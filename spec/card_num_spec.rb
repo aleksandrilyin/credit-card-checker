5f47ad552587cb65e10c56754dbc528638375cc5
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-05 22:22:55 +0300

    Rename subject method to card_num_spec.rb

--- spec/card_num_spec.rb
+++ spec/card_num_spec.rb
@@ -1,20 +1,20 @@
 require 'card_num'
 
 describe CardNum do
-  subject(:cn) { described_class.new(%w[4408 0412 3456 7893]) }
+  subject(:cn1) { described_class.new(%w[4408 0412 3456 7893]) }
 
   describe '#get_begin_digits' do
     context 'when get begin digits' do
       it 'returns four digits' do
-        expect(cn.get_begin_digits(0..3)).to eq('4408')
+        expect(cn1.get_begin_digits(0..3)).to eq('4408')
       end
 
       it 'returns two digits' do
-        expect(cn.get_begin_digits(0..1)).to eq('44')
+        expect(cn1.get_begin_digits(0..1)).to eq('44')
       end
 
       it 'returns the first digit' do
-        expect(cn.get_begin_digits(0)).to eq('4')
+        expect(cn1.get_begin_digits(0)).to eq('4')
       end
     end
   end
