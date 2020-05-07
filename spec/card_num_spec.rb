fbf1746064da4ebb3e678f53805265daf1e36cae
Alexander Ilyin <alexander.ilyin11@gmail.com>
2020-05-07 23:58:18 +0300

    Add specs to card_num_spec.rb

--- spec/card_num_spec.rb
+++ spec/card_num_spec.rb
@@ -18,4 +18,33 @@ describe CardNum do
       end
     end
   end
+
+  describe '#check_card' do
+    context 'when raise ArgumentError' do
+      it 'contains empty card number' do
+        cn = described_class.new([])
+        expect { cn.check_card }.to raise_error(ArgumentError)
+      end
+  
+      it 'contains non-digital card number' do
+        cn = described_class.new(%w[4408 0412 3456 789a])
+        expect { cn.check_card }.to raise_error(ArgumentError)
+      end
+  
+      it 'contains card number length less 13 digits' do
+        cn = described_class.new(%w[4408 0412 3456])
+        expect { cn.check_card }.to raise_error(ArgumentError)
+      end
+  
+      it 'contains card number length 14 digits' do
+        cn = described_class.new(%w[4408 0412 3456 78])
+        expect { cn.check_card }.to raise_error(ArgumentError)
+      end
+  
+      it 'contains card number length more 16 digits' do
+        cn = described_class.new(%w[4408 0412 3456 7893 0])
+        expect { cn.check_card }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
